20041f291cea1817a75a5bb4d0ee2268350f50db
Daniel Videla <daniel.videla.developer@gmail.com>
2022-03-31 19:15:15 -0300

    mejora del ejercicio 9 para que guarde y borre los objetos en el localStorage

--- ejercicio9js/scrip.js
+++ ejercicio9js/scrip.js
@@ -1,23 +1,51 @@
-window.onload=iniciar;
+window.onload = iniciar;
 
 function iniciar() {
     //boton agregar escucha el click
-    var btnAgregar =document.getElementById("btnAgregar");
+    var btnAgregar = document.getElementById("btnAgregar");
     btnAgregar.addEventListener("click", clickBtnAgregar);
+
+    //boton borrar escucha el click
+    var btnBorrar = document.getElementById("btnBorrar");
+    btnBorrar.addEventListener("click", clickBtnBorrar);
+
     //renderizar la nota
     mostrarNotas()
 }
 function clickBtnAgregar() {
- var txtNota = document.getElementById("txtNota");
- localStorage.nota= txtNota.value
- var divNotas=document.getElementById("divNotas");
- divNotas.innerHTML= txtNota.value;
-  mostrarNotas()
+    //traemos txt notas
+    var txtNota = document.getElementById("txtNota");
+     
+    var notas =[];
+    if (localStorage.notas){ 
+    notas = JSON.parse(localStorage.notas);
+    }
+     notas.push(txtNota.value)
+     localStorage.notas= JSON.stringify(notas);
+    mostrarNotas()
 }
+
+/////////////////////////
+function clickBtnBorrar() {
+    localStorage.clear();
+    mostrarNotas()
+    alert("Todos los datos fueron borrados");
+    
+}
+
+
 function mostrarNotas() {
     //trae el div notas
-    var divNotas=document.getElementById("divNotas");
+    var divNotas = document.getElementById("divNotas");
     //le asigna las notas desde el localStorage (almacenamiento local)
-    divNotas.innerHTML= localStorage.nota;
- 
+    var notas =[];
+    if (localStorage.notas){ 
+    notas = JSON.parse(localStorage.notas);
+    }
+    var html="";
+    for (var nota of notas) {
+        html += nota + "<br>";
+    }
+    divNotas.innerHTML = html;
+
 }
